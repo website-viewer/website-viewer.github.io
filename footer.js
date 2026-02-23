031cb199dd0dd96545f93e5752123f1e011ffc59
website-viewer <nikedino121@gmail.com>
2026-02-23 22:19:11 +0500

    Update footer.js

--- footer.js
+++ footer.js
@@ -29,8 +29,9 @@ function renderFooter() {
                     <h4>Resources</h4>
                     <ul>
                         <li><a href="./">Home</a></li>
-                        <li><a href="#features">Features</a></li>
-                        <li><a href="https://github.com/website-viewer" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                        <li><a href="/privacy-policy">Privacy Policy</a></li>
+                        <li><a href="/terms-of-use">Terms of Use</a></li>
                     </ul>
                 </div>
                 <div class="footer-section">
